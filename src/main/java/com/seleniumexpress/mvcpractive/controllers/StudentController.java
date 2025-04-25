ace1c11a59cfff73d0a2f62b38dec8414ae34a78
Amit <aswaraj78@gmail.com>
2025-04-25 19:06:23 +0530

    Added Project, USe XML Approrach for Spring MVC

---
+++ src/main/java/com/seleniumexpress/mvcpractive/controllers/StudentController.java
@@ -0,0 +1,15 @@
+package com.seleniumexpress.mvcpractive.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class StudentController {
+
+	@ResponseBody
+	@GetMapping("/home")
+	public String showStudentHomePage() {
+		return "now you are seeing a student home page";
+	}
+}
